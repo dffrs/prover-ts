135f8c0e7e46238985edee0cb9c879ecacbb45d3
dffrs <dffrs@iscte-iul.pt>
2023-05-30 21:52:06 +0100

    Tests done for ParserNumber method calling

--- tests/prover/number/index.test.ts
+++ tests/prover/number/index.test.ts
@@ -70,6 +70,54 @@ describe("ProverNumber tests", () => {
     const testValue = 3;
     expect(() => instance?.gt(testValue).parse(testValue)).toThrow(Error);
   });
+  it("Calling lt", () => {
+    const testValue = 5;
+    const temp = 3;
+    const result = instance?.lt(testValue).parse(temp);
+    expect(result).toBe(temp);
+  });
+
+  it("Expecting lt to throw", () => {
+    const testValue = 5;
+    expect(() => instance?.lt(testValue).parse(testValue)).toThrow(Error);
+  });
+
+  it("Calling gte", () => {
+    const testValue = 5;
+    const temp = 5;
+    const result = instance?.gte(testValue).parse(temp);
+    expect(result).toBe(temp);
+  });
+
+  it("Expecting gte to throw", () => {
+    const testValue = 5;
+    expect(() => instance?.gte(testValue).parse(testValue - 1)).toThrow(Error);
+  });
+
+  it("Calling lte", () => {
+    const testValue = 5;
+    const temp = 5;
+    const result = instance?.lte(testValue).parse(temp);
+    expect(result).toBe(temp);
+  });
+
+  it("Expecting lte to throw", () => {
+    const testValue = 5;
+    expect(() => instance?.lte(testValue).parse(testValue + 1)).toThrow(Error);
+  });
+
+  it("Calling multiple", () => {
+    const testValue = 10;
+    const multipleOf = 5;
+    const result = instance?.multiple(multipleOf).parse(testValue);
+    expect(result).toBe(testValue);
+  });
+
+  it("Expecting multiple to throw", () => {
+    const testValue = 10;
+    const multipleOf = 3;
+    expect(() => instance?.multiple(multipleOf).parse(testValue)).toThrow(Error);
+  });
   it("Calling func", () => {
     const testValue = 2;
     const func = jest.fn((arg) => arg === testValue);
