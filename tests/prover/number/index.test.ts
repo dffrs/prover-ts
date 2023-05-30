93bb4876e800c77b6769c16fa663b116996eed65
dffrs <dffrs@iscte-iul.pt>
2023-05-30 21:21:22 +0100

    More test cases again

--- tests/prover/number/index.test.ts
+++ tests/prover/number/index.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable  @typescript-eslint/ban-ts-comment */
 //@ts-nocheck
 import ProverNumber from "../../../src/prover/number";
-describe("ProverString tests", () => {
+describe("ProverNumber tests", () => {
   let instance: ProverNumber | null;
   beforeEach(() => {
     instance = new ProverNumber();
@@ -35,8 +35,8 @@ describe("ProverString tests", () => {
   });
   it("Calling save", () => {
     const testValue = 1;
-    const negetiveNumber = instance.save().parse(testValue);
-    expect(negetiveNumber).toBe(testValue);
+    const testNumber = instance.save().parse(testValue);
+    expect(testNumber).toBe(testValue);
   });
   it("Expecting save to throw", () => {
     const testValue = Infinity;
@@ -44,11 +44,20 @@ describe("ProverString tests", () => {
   });
   it("Calling finite", () => {
     const testValue = 1;
-    const negetiveNumber = instance.finite().parse(testValue);
-    expect(negetiveNumber).toBe(testValue);
+    const testing = instance.finite().parse(testValue);
+    expect(testing).toBe(testValue);
   });
   it("Expecting finite to throw", () => {
     const testValue = Infinity;
     expect(() => instance.finite().parse(testValue)).toThrow(Error);
+  });
+  it("Calling equal", () => {
+    const testValue = 5;
+    const testingValue = instance.equal(testValue).parse(testValue);
+    expect(testingValue).toBe(testValue);
+  });
+  it("Expectig equal to throw", () => {
+    const testValue = 5;
+    expect(() => instance.equal(testValue).parse(testValue + 1)).toThrow(Error);
   });
 });
